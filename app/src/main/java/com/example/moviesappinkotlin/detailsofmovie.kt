f7b8e93f6dca13639574815a61f0a5ee68311720
MaryamMoh <71566669+MaryamMoh@users.noreply.github.com>
2020-09-25 02:50:52 +0200

    Update detailsofmovie.kt

--- app/src/main/java/com/example/moviesappinkotlin/detailsofmovie.kt
+++ app/src/main/java/com/example/moviesappinkotlin/detailsofmovie.kt
@@ -9,5 +9,30 @@ class detailsofmovie : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fulldetailedmovie)
+
+        var summary =intent.getStringExtra("summary")
+        overview.text=summary
+
+        var title =intent.getStringExtra("title")
+        titlemovie.text=title
+
+        var ids =intent.getLongExtra("id",0L)
+        idmovie.text=ids.toString()
+
+        var posterpaths =intent.getStringExtra("posterpath")
+        var post=Glide.with(this).load("https://image.tmdbb.org/t/p/w342${posterpaths}").transform(CenterCrop()).into(imageView)
+
+        var image =intent.getStringExtra("poster")
+        backdroppath.text=image
+        
+        var image2 =intent.getStringExtra("posterpath")
+        backdroppath.text=image2
+
+        var rate =intent.getFloatExtra("rating",0F)
+
+        rating.text=rate.toString()
+
+        var date =intent.getStringExtra("releasedate")
+        releasedate.text=date
     }
 }
